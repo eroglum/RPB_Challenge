7077f5631cb02b9f128bde4351415e4a733fe559
eroglu.m01@htl-ottakring.ac.at <eroglu.m01@htl-ottakring.ac.at>
2021-11-04 14:43:23 +0100

    Version 2.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <slider/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import slider from './components/slider';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    slider,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/slider.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-card
+    class="mx-auto"
+  >
+    
+
+    <v-container>
+        <div class="headline">Länder, Regionen, Orte
+            <v-btn color="#16828F" :right="true" :absolute="true" text>Alle</v-btn>
+        </div>
+        <div class="subtitle-1">Lorem ipsum Länder, Regionen, Orte</div>
+      <v-row dense>
+        <v-slide-group multiple class="pa-4" :show-arrows="false">
+          <v-slide-item v-for="(item, i) in items"
+          :key="i">
+        
+          <v-card id="karte" class="rounded-xl mx-2" border-color="lime" outlined>
+            
+                <v-img :src="item.image"></v-img>
+                <a :href=item.link><v-card-text class="text--primary">
+      <v-row>
+        <v-col class="d-flex justify-space-between subtitle-2">{{item.text}}</v-col>
+        <v-col class="text-right h6"><span class="caption">ab</span> € {{item.price}}</v-col>
+      </v-row>
+    </v-card-text>
+    </a>
+          </v-card>
+          
+        </v-slide-item>
+        </v-slide-group>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+{
+"text": "Ägypten",
+"price": 1237,
+"image": "https://images.flux.reisen/hotel/1262/1/s",
+"link": "https://google.com"
+},
+{
+"text": "Griechenland",
+"price": 334,
+"image": "https://images.flux.reisen/hotel/1262/1/s",
+"link": "https://google.com"
+},
+{
+"text": "Türkei",
+"price": 237,
+"image": "https://images.flux.reisen/hotel/1262/1/s",
+"link": "https://google.com"
+},
+{
+"text": "Italien",
+"price": 237,
+"image": "https://images.flux.reisen/hotel/1262/1/s",
+"link": "https://google.com"
+},
+{
+"text": "Spanien",
+"price": 379,
+"image": "https://images.flux.reisen/hotel/1262/1/s",
+"link": "https://google.com"
+},
+{
+"text": "Kroatien",
+"price": 88,
+"image": "https://images.flux.reisen/hotel/1262/1/s",
+"link": "https://google.com"
+},
+{
+"text": "Österreich",
+"price": 0,
+"image": "https://images.flux.reisen/hotel/1262/1/s",
+"link": "https://google.com"
+}
+],
+    }),
+  }
+</script>
+
+<style>
+
+#karte {
+    box-shadow: -2px 2px 1px #D9E15F;
+    background: linear-gradient(white)
+}
+a {
+  text-decoration: none !important;
+}
+
+</style>
